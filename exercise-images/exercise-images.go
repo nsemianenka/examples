c0932ab9cbd0c7d8a6addeeeaf5b781d1974aeea
nsemianenka <nikitka7@bk.ru>
2017-02-17 00:33:26 +0300

    exercise emages

---
+++ exercise-images/exercise-images.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct{
+	w, h int
+}
+
+func (img Image) ColorModel() (color.Model) {
+	return color.RGBAModel
+}
+
+func (img Image) Bounds() (image.Rectangle) {
+	left, right := image.Point {0,0}, image.Point{img.w, img.h}
+	return image.Rectangle{left, right}
+}
+
+func (img Image) At(x, y int) (color.Color) {
+	v := uint8((x^y)/2)
+	return color.RGBA{v, v, 255,255}
+}
+
+func main() {
+	m := Image{500, 500}
+	pic.ShowImage(m)
+}
+
