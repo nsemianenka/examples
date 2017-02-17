03c4050261cf4a040e10c423fa3964b4a90ed34f
nsemianenka <semenenko.nikita@tut.by>
2017-02-17 19:07:47 +0300

    goroutine equivalint binary trees exercise

---
+++ exercise-equivalent-binery-trees/exercise-equivalent-binery-trees.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"golang.org/x/tour/tree"
+	"fmt"
+)
+
+func walkTreeRecurse(t *tree.Tree, ch chan int) {
+	if t.Left != nil {
+		walkTreeRecurse(t.Left, ch)
+	}
+	ch <- t.Value
+	if t.Right != nil {
+		walkTreeRecurse(t.Right, ch)
+	}
+}
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	walkTreeRecurse(t, ch)
+	close(ch)
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1, ch2 := make(chan int), make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := range ch1 {
+		if i != <- ch2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(Same(tree.New(100), tree.New(100)))
+}
