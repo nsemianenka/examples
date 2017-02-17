c6bea99003a74e57988d4382a328372c4bbe23ed
nsemianenka <semenenko.nikita@tut.by>
2017-02-17 19:31:14 +0300

    add check on close for second channel

--- exercise-equivalent-binery-trees/exercise-equivalent-binery-trees.go
+++ exercise-equivalent-binery-trees/exercise-equivalent-binery-trees.go
@@ -25,13 +25,22 @@ func Walk(t *tree.Tree, ch chan int) {
 // Same determines whether the trees
 // t1 and t2 contain the same values.
 func Same(t1, t2 *tree.Tree) bool {
-	ch1, ch2 := make(chan int), make(chan int)
+	ch1, ch2, ch2Ok, j := make(chan int), make(chan int), true, 0
 	go Walk(t1, ch1)
 	go Walk(t2, ch2)
 	for i := range ch1 {
-		if i != <- ch2 {
+		if ch2Ok {
+			j, ch2Ok = <-ch2
+			if i != j {
+				return false
+			}
+		} else {
 			return false
 		}
+	}
+	_, ch2Ok = <-ch2
+	if ch2Ok {
+		return false
 	}
 	return true
 }
